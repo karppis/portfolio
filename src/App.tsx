ee021ab13ca7c15d294c0c812c20d306be6fd090
karppis <skarppinen2000@gmail.com>
2022-11-08 09:56:00 +0200

    clickcounter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 // App.tsx
 import React from 'react'
 import MyFirstComponent from './components/myFirstComponent';
+import SomeExampleComponent from './components/example/someExampleComponent';
+import ClickCount from './clickCount';
 
 const App: React.FC = () => {
   return (
     <div>
       <MyFirstComponent />
+      <SomeExampleComponent />
+      <ClickCount />
     </div>
   )
 }
